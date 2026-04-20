04c8397b42d349070ad345a64cf6079282d5f377
unta1337 <firstapple42@gmail.com>
2026-04-20 16:40:13 +0900

    exp2에서 다양한 파라미터셋으로 실행하도록 수정

--- hw2/exp2.ps1
+++ hw2/exp2.ps1
@@ -3,3 +3,31 @@ uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg
 
 # Baseline
 uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 5 --exp_name cheetah_baseline
+
+# Baseline - Lower BLR
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.005 -bgs 5 --exp_name cheetah_baseline_lower_blr
+
+# Baseline - Lower BGS
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 2 --exp_name cheetah_baseline_lower_bgs
+
+# Baseline - Lower BLR, BGS
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.005 -bgs 2 --exp_name cheetah_baseline_lower_blr_bgs
+
+## With NA
+# No baseline
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --exp_name cheetah_na -na
+
+# Baseline
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 5 --exp_name cheetah_baseline_na -na
+
+# Baseline - Lower BLR
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.005 -bgs 5 --exp_name cheetah_baseline_lower_blr_na -na
+
+# Baseline - Lower BGS
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 2 --exp_name cheetah_baseline_lower_bgs_na -na
+
+# Baseline - Lower BLR, BGS
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.005 -bgs 2 --exp_name cheetah_baseline_lower_blr_bgs_na -na
+
+# Baseline with NA - Video Log
+uv run src/scripts/run.py --env_name HalfCheetah-v4 -n 100 -b 5000 -eb 3000 -rtg --discount 0.95 -lr 0.01 --use_baseline -blr 0.01 -bgs 5 --exp_name cheetah_baseline_na_video -na --video_log_freq 10
